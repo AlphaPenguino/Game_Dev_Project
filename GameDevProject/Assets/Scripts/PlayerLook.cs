762cd647bec440b88773cf56d99b3bd7410557f3
AlphaPenguino <zunder.pacis@lpunetwork.edu.ph>
2025-03-19 20:55:23 +0800

    Update PlayerLook.cs

--- GameDevProject/Assets/Scripts/PlayerLook.cs
+++ GameDevProject/Assets/Scripts/PlayerLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    public Camera camera;
+    public Camera cam;
     private float xRotation = 0f;
     public float xSensitivity = 30f;
     public float ySensitivity = 30f;
@@ -17,7 +17,7 @@ public class PlayerLook : MonoBehaviour
 
         xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
-        camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
     }
 }
